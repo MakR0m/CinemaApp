56df1187607bfb03e507b946db73d24ad2722279
Роман Макаров <makarovri@edu.stu.ru>
2025-01-14 16:44:22 +0700

    Добавлена модель Movie с основными свойствами

---
+++ CinemaApp.Core/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaApp.Core.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public string Description { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string PosterUrl { get; set; }
+    }
+}
