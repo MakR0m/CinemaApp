f5899950f47b7ae0e404bb9d3d3e11cb98e904df
Роман Макаров <makarovri@edu.stu.ru>
2025-01-14 16:45:47 +0700

    Определен интрефейс IMovieRepository для управления фильмами

---
+++ CinemaApp.Core/Interfaces/IMovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using CinemaApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Core.Interfaces
+{
+    public interface IMovieRepository
+    {
+        public Task<IEnumerable<Movie>> GetAllAsync();
+        public Task<Movie> GetByIdAsync(int id);
+        public Task AddAsync(Movie movie);
+        public Task UpdateAsync(Movie movie);
+        public Task DeleteAsync(int id);
+    }
+}
