a31c879c0d2253fce3a33d7f814b625d7c8923c0
Роман Макаров <makarovri@edu.stu.ru>
2025-01-15 14:02:14 +0700

    Добавлен вьюмодел для афиши фильмов: PosterViewModel

---
+++ CinemaApp.UI/ViewModels/PosterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CinemaApp.Core.Interfaces;
+using CinemaApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.UI.ViewModels
+{
+    public class PosterViewModel
+    {
+        private readonly IMovieRepository _movieRepository;  // Данное поле нужно, чтобы вьюмодел работала с данными не зная откуда они берутся.
+                                                             // Соответствует принципу внедрения зависимосимостей (Dependency Injection)
+                                                             // и призципу разделения ответственности (Separaion of Concerns
+
+        ObservableCollection<Movie> Movies { get; set; }
+
+        public PosterViewModel(IMovieRepository repository)
+        {
+            _movieRepository = repository;
+            Movies = new ObservableCollection<Movie>();
+        }
+
+        public async Task LoadMoviesAsnync()
+        {
+            var movies = await _movieRepository.GetAllAsync();
+            Movies.Clear();
+            foreach (var movie in movies)
+            {
+                Movies.Add(movie);
+            } 
+        }
+
+    }
+}
